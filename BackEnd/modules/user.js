ae7926c4816b587c62488673e512ea9548f4c12d
KariR61 <kariraappana@gmail.com>
2016-01-11 14:47:26 +0200

    new files

--- BackEnd/modules/user.js
+++ BackEnd/modules/user.js
@@ -1,5 +1,5 @@
 var query = require('./queries');
-
+var mysql = require('./mysql_module');
 /*
 *This file is a router for User resource
 *Version:0.0.1
@@ -19,15 +19,17 @@ router.get('/',function(req,res){
 //This router handles a request to url localhost:3000/friends/login
 router.post('/login',function(req,res){
     
-    query.loginFriend(req,res);
+    //query.loginFriend(req,res);
+    mysql.loginMysqlProc(req,res);
 });
 
 
 //This router handles a request to url localhost:3000/friends/register
 router.post('/register',function(req,res){
     
-    query.registerFriend(req,res);
+   query.registerFriend(req,res);
     
+    //mysql.loginMysql(req,res);
 });
 
 module.exports = router;
